fee7670721cfa87eac54df57431cf8f3c3c392f7
Connell Webb (1000104559) <WEBBCE2@student.op.ac.nz>
2022-11-01 16:43:12 +1300

    Added Controller, and Enemy Fleet

--- SpaceInvadersGame/Form1.cs
+++ SpaceInvadersGame/Form1.cs
@@ -2,9 +2,26 @@ namespace SpaceInvadersGame
 {
     public partial class Form1 : Form
     {
+        private Bitmap bufferImage;
+        private Graphics bufferGraphics;
+        private Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+
+            Width = 800;
+            Height = 600;
+
+            bufferImage = new Bitmap(Width, Height);
+
+            bufferGraphics = Graphics.FromImage(bufferImage);
+
+            graphics = CreateGraphics();
+
+            controller = new Controller(ClientSize, bufferGraphics);
+
+            timer1.Enabled = true;
         }
 
         public void Draw()
